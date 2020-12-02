35eae27087db1694e6cbae7b790126fcf0859509
Jinjo-bit <71643734+Jinjo-bit@users.noreply.github.com>
2020-12-02 12:59:40 -0500

    Add files via upload

---
+++ operators/lab1.js
@@ -0,0 +1,19 @@
+var secondsInAMinute = 60;
+var minutesInAHour = 60;
+var hoursInADay = 24;
+var daysInAYear = 365;
+
+var secondsInAHour = secondsInAMinute * minutesInAHour;
+document.write("the number of seconds in an hour is" + secondsInAHour);
+document.write("<br>")
+
+var secondsInADay = secondsInAHour * hoursInADay;
+document.write("the number of seconds in an day is" + secondsInADay);
+document.write("<br>");
+
+var secondsInAyear = secondsInADay * daysInAYear;
+document.write("the number of seconds in an year is" + secondsInAyear);
+document.write("<br>");
+document.write("<br>");
+document.write("Created by JaQuez");
+
